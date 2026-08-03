4356b6c4bd09f7255143903d34f9854f0c222bf0
Valmur Prado <valmur.prado.external@sosafe.de>
2026-08-03 16:04:04 +0100

    fix name of fields

--- dbt/models/pit/pit_investidor.sql
+++ dbt/models/pit/pit_investidor.sql
@@ -5,21 +5,26 @@
 select
 
     h.hubInvestidorHk,
-
     h.codigoDoInvestidor,
 
-    s.nome,
-    s.uf,
-    s.cidade,
-    s.perfilInvestidor,
+    s.dataDeAdesao,
+    s.estadoCivil,
+    s.genero,
+    s.profissao,
+    s.idade,
+    s.ufDoInvestidor,
+    s.cidadeDoInvestidor,
+    s.paisDoInvestidor,
+    s.situacaoDaConta,
+    s.operou12Meses,
+
     s.loadTs,
+    s.loadDate,
 
     current_timestamp() as pitLoadTs,
-
     current_date() as pitLoadDate
 
 from {{ source('silver', 'hubInvestidor') }} h
 
 left join {{ ref('int_sat_investidor_atual') }} s
-
     on h.hubInvestidorHk = s.hubInvestidorHk
